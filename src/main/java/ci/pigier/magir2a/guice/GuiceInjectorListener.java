c6a357f31f627113cc163669c29e83727c267a37
moi-digbe <40300284+moi-digbe@users.noreply.github.com>
2020-02-21 16:43:01 +0000

    Add files via upload

---
+++ src/main/java/ci/pigier/magir2a/guice/GuiceInjectorListener.java
@@ -0,0 +1,34 @@
+package ci.pigier.magir2a.guice;
+
+import ci.pigier.magir2a.service.MicroserviceModule;
+import ci.pigier.magir2a.service.StorageModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.ctoolkit.services.endpoints.EndpointsMonitorConfig;
+import org.ctoolkit.services.guice.AppEngineEnvironmentContextListener;
+
+/**
+ * The main entry point to configure guice injection.
+ *
+ * @author <a href="mailto:medvegy@turnonline.biz">Aurel Medvegy</a>
+ */
+public class GuiceInjectorListener
+        extends AppEngineEnvironmentContextListener
+{
+    @Override
+    protected Injector getDevelopmentInjector()
+    {
+        return Guice.createInjector( new MicroserviceModule(),
+                new StorageModule(),
+                new EndpointsInitialization() );
+    }
+
+    @Override
+    protected Injector getProductionInjector()
+    {
+        return Guice.createInjector( new MicroserviceModule(),
+                new StorageModule(),
+                new EndpointsInitialization(),
+                new EndpointsMonitorConfig() );
+    }
+}
